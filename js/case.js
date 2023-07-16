70cb3c13d7d6373a0a7975238ddc433059f69b0c
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-17 01:58:07 +0600

    case number updated

--- js/case.js
+++ js/case.js
@@ -9,28 +9,31 @@ function updateCaseQuantityById(isIncrease) {
   } else {
     newCaseQuantity = caseQuantity - 1;
   }
+
+  if (newCaseQuantity < 0) {
+    alert("Case number must not be less than zero (0)!");
+    return false;
+  }
+  // In JavaScript “0” is equal to false because “0” is of type string but when it tested for equality the automatic type conversion of JavaScript comes into effect and converts the “0” to its numeric value which is 0 and as we know 0 represents false value. So, “0” equals to false.
+
   caseNumber.value = newCaseQuantity;
   return newCaseQuantity;
 }
 
-// function casePriceTotalById() {
-//   const casePerPrice = document.getElementById("case-total");
-//   const casePriceTotalString = casePerPrice.innerText;
-//   const casePriceTotal = parseInt(casePriceTotalString);
-//   casePerPrice.innerText = casePriceTotal;
-//   return casePriceTotal;
-// }
+function casePriceTotalById(increaseOrDecrease) {
+  const caseTotalPrice = increaseOrDecrease * 59;
+  const caseTotalElement = document.getElementById("case-total");
+  caseTotalElement.innerText = caseTotalPrice;
+}
 
 document.getElementById("btn-plus-case").addEventListener("click", function () {
   const increaseCaseNumber = updateCaseQuantityById(true);
-
-  const caseTotalPrice = increaseCaseNumber * 59;
-  const caseTotalElement = document.getElementById("case-total");
-  caseTotalElement.innerText = caseTotalPrice;
+  casePriceTotalById(increaseCaseNumber);
 });
 
 document
   .getElementById("btn-minus-case")
   .addEventListener("click", function () {
-    updateCaseQuantityById(false);
+    const decreaseCaseNumber = updateCaseQuantityById(false);
+    casePriceTotalById(decreaseCaseNumber);
   });
